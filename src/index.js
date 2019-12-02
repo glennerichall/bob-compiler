c9f77cb697ea89392f42cebc51df2cf6f3adc6fc
glenn-eric <glennerichall@hotmail.com>
2019-12-02 09:56:35 -0500

    closes #21

--- src/index.js
+++ src/index.js
@@ -8,32 +8,37 @@ import NpmApi from 'npm-api';
 
 // ---------------------------------------------------------------------------
 version().then(value => {
-  let argv = yargs
-    .scriptName('bobc')
-    .usage('$0 <cmd> [args]')
-    .version(value)
-    .command(...lstCmd)
-    .command(...preCmd)
-    .command(...cpmCmd)
-    .wrap(yargs.terminalWidth())
-    .demandCommand(1, '')
-    .strict()
-    .showHelpOnFail(true)
-    .help().argv;
+  try {
+    let argv = yargs
+      .scriptName('bobc')
+      .usage('$0 <cmd> [args]')
+      .version(value)
+      .command(...lstCmd)
+      .command(...preCmd)
+      .command(...cpmCmd)
+      .wrap(yargs.terminalWidth())
+      .demandCommand(1, '')
+      .strict()
+      .showHelpOnFail(true)
+      .exitProcess(false)
+      .help().argv;
 
-  if (argv.verbose) {
-    console.log('\nCurrent effective options are:');
-    console.log(argv);
-  }
-
-  let npm = new NpmApi();
-  var repo = npm.repo('bob-compiler');
-
-  repo.package().then(pkg => {
-    if ('v' + pkg.version != value) {
-      console.warn(
-        `Newer version available ${pkg.version}, consider upgrading it`
-      );
+    if (argv.verbose) {
+      console.log('\nCurrent effective options are:');
+      console.log(argv);
     }
-  });
+
+    let npm = new NpmApi();
+    var repo = npm.repo('bob-compiler');
+
+    repo.package().then(pkg => {
+      if ('v' + pkg.version != value) {
+        console.warn(
+          `Newer version available ${pkg.version}, consider upgrading it`
+        );
+      }
+    });
+  } catch (e) { }
+
+
 });
