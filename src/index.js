e10d58c80444a8bab5f3e0cf66036531e19e879c
Glenn Hall <glennerichall@hotmail.com>
2020-10-08 15:34:04 -0400

    fixed version check

--- src/index.js
+++ src/index.js
@@ -9,40 +9,42 @@ import logger from './logger.js';
 import { levels } from './logger.js';
 
 // ---------------------------------------------------------------------------
-version().then(value => {
-  try {
-    let argv = yargs
-      .scriptName('bobc')
-      .usage('$0 <cmd> [args]')
-      .version(value)
-      .command(...lstCmd)
-      .command(...preCmd)
-      .command(...cpmCmd)
-      .wrap(yargs.terminalWidth())
-      .demandCommand(1, '')
-      .strict()
-      .showHelpOnFail(true)
-      .exitProcess(false)
-      .help().argv;
+version()
+  .then((value) => {
+    try {
+      let argv = yargs
+        .scriptName('bobc')
+        .usage('$0 <cmd> [args]')
+        .version(value)
+        .command(...lstCmd)
+        .command(...preCmd)
+        .command(...cpmCmd)
+        .wrap(yargs.terminalWidth())
+        .demandCommand(1, '')
+        .strict()
+        .showHelpOnFail(true)
+        .exitProcess(false)
+        .help().argv;
 
-    if (argv.verbose) {
-      logger.level = levels.info;
-    }
+      if (argv.verbose) {
+        logger.level = levels.info;
+      }
 
-    logger.info('\nCurrent effective options are:');
-    logger.info(argv);
+      logger.info('\nCurrent effective options are:');
+      logger.info(argv);
+    } catch (e) {}
 
     let npm = new NpmApi();
     var repo = npm.repo('bob-compiler');
-
-    repo.package().then(pkg => {
-      if ('v' + pkg.version != value) {
-        console.warn(
-          `Newer version available ${pkg.version}, consider upgrading it`
-        );
-      }
-    }).catch(() => { });
-  } catch (e) { }
-
-
-});
+    repo
+      .package()
+      .then((pkg) => {
+        if ('v' + pkg.version != value) {
+          console.warn(
+            `Newer version available ${pkg.version}, consider upgrading it`
+          );
+        }
+      })
+      .catch(() => {});
+  })
+  .catch(() => {});
